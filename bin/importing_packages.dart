ab7fbf0be3f3fddc14b15defe982ce5694e174e6
Victor Teodoro <victorjst@hotmail.com>
2021-05-29 15:01:04 -0300

    examples: add package importing example

---
+++ bin/importing_packages.dart
@@ -0,0 +1,6 @@
+import 'dart:math' as math;
+
+Future<void> main(List<String> arguments) async {
+  print('e constant: ${math.e.toString()}');
+  print('e constant rounded: ${math.e.toStringAsFixed(6)}');
+}
